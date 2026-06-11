4339b3c3338a8840f3943ed39641fe2639d9dbfc
yaxhcodes <yaxhcodes@users.noreply.github.com>
2026-06-11 16:26:50 +0530

    fixed notifications while a url got processed and saved on prod

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.shinrinyoku.glimpse"
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
-        versionCode = 21
+        versionCode = 26
         versionName = flutter.versionName
     }
 
