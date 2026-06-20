8a19b0f5396d5e08d1d3cda0f368b63fda9c8921
yaxhcodes <yaxhcodes@users.noreply.github.com>
2026-06-20 22:57:15 +0530

    Prepare Android release dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.shinrinyoku.glimpse"
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
-        versionCode = 31
+        versionCode = 34
         versionName = flutter.versionName
     }
 
