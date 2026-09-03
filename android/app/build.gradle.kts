b630396ea33f06fd9cae882844809de3641ee858
yaxhcodes <yaxhcodes@users.noreply.github.com>
2026-09-03 11:27:30 +0530

    chore: bump Android version code to 138

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.shinrinyoku.glimpse"
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
-        versionCode = 114
+        versionCode = 138
         versionName = flutter.versionName
     }
 
